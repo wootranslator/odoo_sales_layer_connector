696169faa495cf1a3cceee12e66c6ee5cb836cc0
Javier Sánchez <sanchezdepedro85@gmail.com>
2026-01-23 13:20:42 +0100

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Sales Layer PIM Connector',
+    'version': '18.0.1.0.0',
+    'category': 'Inventory/Inventory',
+    'summary': 'Sincronización de variantes con Sales Layer PIM',
+    'author': 'Tu Nombre/Empresa',
+    'depends': ['product', 'mail'],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
